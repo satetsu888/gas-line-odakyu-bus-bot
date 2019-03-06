6eb96a3967ee484c2e571e337597d9b6ea9118b4
satetsu888 <satetsu888@gmail.com>
2019-03-06 22:55:39 +0900

    version up deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ clasp versions > .versions
 LAST_VERSION=$(cat .versions | grep -v "~" | head -n 1 | cut -d " " -f 1)
 DEPLOYMENT_ID=`clasp deployments | grep "web app meta-version" | cut -d " " -f 2`
 echo deploy @${LAST_VERSION} to ${DEPLOYMENT_ID}
-clasp redeploy ${DEPLOYMENT_ID} ${LAST_VERSION} "web app meta-version"
+clasp deploy --versionNumber ${LAST_VERSION} --deploymentId ${DEPLOYMENT_ID} --description "web app meta-version"
